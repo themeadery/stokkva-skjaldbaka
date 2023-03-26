55cf431124b5e4552796a08e6423451c9b8ca88e
Matt Mead <matt.a.mead@gmail.com>
2023-03-26 16:56:49 -0700

    Initial Milestone
    Tested and working on my X-T1

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# WIP
+# støkkva skjaldbaka
+
+støkkva skjaldbaka translates to "Snapping Turtle" in Old Norse, or at least Icelandic (which is apparently similar)
+
+"Snap" because the program triggers the camera to snap photos  
+"Turtle" because timelapses are slow and take a lot of time
+
+## Idea/Workflow
+
+Capture an image and transfer -> Measure brightness -> Adjust settings -> Capture another image -> repeat
+
+## Why?
+
+- The X-T1 bult-in intervalometer is limited to 999 shots
+- You don't want to buy a bigger memory card
+- You don't want to buy an expensive intervalometer
+- There is no info on the interface when shooting timelapses, so you can't see your exposure meter or histogram and adjust accurately
+- Conditions are changing during the shoot
+- You want to ramp day-to-night/holy grail
+
+## Requires
+
+- Python 3.x
+- gphoto2
+- exiftool
+- ImageMagick (v7.x recommended)
+- USB cable
+- Change the USB mode to "PC SHOOT AUTO" or "PC SHOOT FIXED" (Recommended)
+
+## Commands
+
+    $ python stokkva.py
+    støkkva skjaldbaka started
+
+## Problems
+
+#### gphoto errors when issuing commands:
+
+    $ ps aux | grep gphoto
+    $ kill -9 {pid of gvfs-gphoto2-volume-monitor}
+or
+
+    $ sudo chmod -x /usr/lib/gvfs/gvfs-gphoto2-volume-monitor
+    $ sudo chmod -x /usr/lib/gvfs/gvfsd-gphoto2
+You probably need to power cycle the camera after fixing these problems
+
+The "PC SHOOT FIXED" USB option in the X-T1 says it will not save to the memory card, but it still does. Take out the memory card if you don't want it to fill up or be the limitation of your duration. Might also speed up the process, since it isn't trying to write data twice.
+
+Your shutter dial needs to be in the "T" position to accept shutter speed change commands.
+
+My X-T1 shuts off pretty quick if it is tethered and no commands are sent. Seems like about 1 minute timeout. Must power cycle the camera to reconnect.
+
+Sometimes the camera refuses to shut off while still plugged in to USB.
+
+## Credit
+
+Based on <https://github.com/dps/rpi-timelapse>  
+I did not fork his project because it is more complicated than I needed and it is based on python2.
+
+## TODO
+
+- ISO ramping - Doesn't respond to the --set-config command
---
+++ stokkva.py
@@ -0,0 +1,112 @@
+from datetime import datetime
+from datetime import timedelta
+import time
+from itertools import count
+import subprocess
+
+MIN_BRIGHTNESS = 20000
+MAX_BRIGHTNESS = 30000
+
+def reset():
+    print("Resetting connection to camera...")
+    subprocess.run(["gphoto2", "--reset"])
+    print("Connection reset")
+
+def get_shutter_speed():
+    print("Getting Shutter Speeds...")
+    out = subprocess.check_output(["gphoto2", "--get-config", "shutterspeed"])
+    out = out.decode()
+    #print(out) #Debug
+    shutter_choices = {}
+    for line in out.split('\n'):
+        if line.startswith('Choice:'):
+            shutter_choices[line.split(' ')[2]] = line.split(' ')[1]
+        if line.startswith('Current:'):
+            shutter_current_choice = line.split(' ')[1]
+    return shutter_current_choice, shutter_choices
+
+def get_iso():
+    print("Getting ISO Speeds...")
+    out = subprocess.check_output(["gphoto2", "--get-config", "iso"])
+    out = out.decode()
+    #print(out) #Debug
+    iso_choices = {}
+    for line in out.split('\n'):
+        if line.startswith('Choice:'):
+            iso_choices[line.split(' ')[2]] = line.split(' ')[1]
+        if line.startswith('Current:'):
+            iso_current_choice = line.split(' ')[1]
+    return iso_current_choice, iso_choices
+
+def capture_image_and_download(filename):
+    print("\nCapturing Image...")
+    out = subprocess.check_output(["gphoto2", "--capture-image-and-download", "--force-overwrite", "--filename", filename])
+    print("Downloading...")
+    out = out.decode()
+    for line in out.split("\n"):
+        if line.startswith("Saving file as "):
+            filename = line.split("Saving file as ")[1]
+    print(f"Downloaded image {filename}")
+    return filename
+
+def measure_brightness(filename):
+    print(f"Measuring brightness of {filename}...")
+    print(f"Extracting preview of {filename}...")
+    subprocess.run(["exiftool", "-b", "-PreviewImage", "-w", "_preview.jpg", filename])
+    print("Extracted")
+    preview = filename.strip(".RAF") + "_preview.jpg"
+    out = subprocess.check_output(["identify", "-format", "%[mean]", preview])
+    brightness = float(out.decode())
+    print(f"Brightness is {brightness}")
+    return brightness
+
+def set_shutter_speed(shutter_choice):
+    print(f"Setting shutterspeed to {shutter_choice}...")
+    subprocess.run(["gphoto2", "--set-config", f"shutterspeed={shutter_choice}"]) # Note: will take a choice index or literal
+    print("Shutter speed set")
+
+def set_iso(iso_choice):
+    print(f"Setting ISO to {iso_choice}...")
+    subprocess.run(["gphoto2", "--set-config", f"iso={iso_choice}"])
+    print("ISO set")
+
+# Main
+print("\nstøkkva skjaldbaka started\n")
+reset() # Reset connection to camera just in case
+
+# Get configs
+shutter_current_choice, shutter_choices = get_shutter_speed()
+# TODO: Don't do this below
+# Read the shutter_current_choice from get_shutter_speed()
+# Then match shutter_current_choice with dictionary from shutter_choices because it is a literal speed instead of a "choice"
+# This will allow brightness if statement math below to increment by index of 1
+shutter_choice = 22 # Set initial shutter speed to 1/30
+set_shutter_speed(shutter_choice)
+iso_current_choice, iso_choices = get_iso()
+
+# Set timelapse parameters
+#interval = int(input("Enter Interval in seconds: ")) #Turn on when it's a real interactive program
+interval = 7 # Non interactive for pasting
+interval = timedelta(seconds=interval)
+#length = int(input("Enter Total Number of Shots: ")) #Turn on when it's a real interactive program
+length = 20 # Non interactive for pasting
+
+# Filename iterator
+iterator = ("DSCF%04i.RAF" % i for i in count(1)) # Generate a new filename for each shot, starting with DSCF0001.raf
+
+# Main loop
+for x in range(length):
+    started = datetime.now() # Start timing the operation
+    filename = next(iterator) # Increment shot number by 1
+    filename = capture_image_and_download(filename)
+    brightness = measure_brightness(filename)
+    if brightness < MIN_BRIGHTNESS and shutter_choice < len(shutter_choices) - 1: # check if exceeds interval, requires dictionary lookup
+        shutter_choice = shutter_choice + 1
+        set_shutter_speed(shutter_choice)
+    elif brightness > MAX_BRIGHTNESS and shutter_choice > 0:
+        shutter_choice = shutter_choice - 1
+        set_shutter_speed(shutter_choice)
+    ended = datetime.now() # Stop timing the operation
+    if started and ended and ended - started < interval:
+        print("Sleeping...")
+        time.sleep((interval - (ended - started)).seconds)
